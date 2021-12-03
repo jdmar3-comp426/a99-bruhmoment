632cb06d969a94cfd742f86f086f186c983e3ec9
kianw97 <kianw@live.unc.edu>
2021-12-03 17:03:45 -0500

    login/sign up changes

--- scripts/create_account.js
+++ scripts/create_account.js
@@ -12,11 +12,37 @@ var create_user = (username, password, email)=>{
         body: raw,
         redirect: 'follow'
     };
+    //stores username in local storage for use in score
+    localStorage.setItem("username", username);
     // make API call with parameters and use promises to get response
     fetch("https://bjpskapus9.execute-api.us-east-1.amazonaws.com/dev/app/user/new", requestOptions)
     .then(response => response.text())
     .then(result => {
         // if false prompt user to try again with a new username
         // if true store username as global variable for future use 
+    }).catch(error => console.log('error', error));
+}
+
+var create_score = (username)=>{
+    // instantiate a headers object
+    var myHeaders = new Headers();
+    // add content type header to object
+    myHeaders.append("Content-Type", "application/json");
+    // using built in JSON utility package turn object to string and store in a variable
+    var raw = JSON.stringify({"username":username});
+    // create a JSON object with parameters for API call and store in a variable
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+    // make API call with parameters and use promises to get response
+    fetch("https://bjpskapus9.execute-api.us-east-1.amazonaws.com/dev/app/score/new", requestOptions)
+    .then(response => response.text())
+    .then(result => {
+        // indicate success or failure to front-end
+        console.log(result);
+        console.log(JSON.parse(result).body.success);
     }).catch(error => console.log('error', error));
 }
--- scripts/login_manager.js
+++ scripts/login_manager.js
@@ -8,12 +8,37 @@ var login = (username, password) => {
         body: raw,
         redirect: 'follow'
     };
-
+    //stores username in local storage for use in score
+    localStorage.setItem("username", username);
     fetch("https://bjpskapus9.execute-api.us-east-1.amazonaws.com/dev/app/user/login", requestOptions)
     .then(response => response.text())
     .then(result => {
         // indicate whether the user was successfully logged in
         // store username as global variable
+        console.log(result);
+        console.log(JSON.parse(result).body.success);
+    }).catch(error => console.log('error', error));
+}
+
+var create_score = (username)=>{
+    // instantiate a headers object
+    var myHeaders = new Headers();
+    // add content type header to object
+    myHeaders.append("Content-Type", "application/json");
+    // using built in JSON utility package turn object to string and store in a variable
+    var raw = JSON.stringify({"username":username});
+    // create a JSON object with parameters for API call and store in a variable
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+    // make API call with parameters and use promises to get response
+    fetch("https://bjpskapus9.execute-api.us-east-1.amazonaws.com/dev/app/score/new", requestOptions)
+    .then(response => response.text())
+    .then(result => {
+        // indicate success or failure to front-end
         console.log(result);
         console.log(JSON.parse(result).body.success);
     }).catch(error => console.log('error', error));
